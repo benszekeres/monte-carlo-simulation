1cd4e1e429be6c714145523c53ccb4d6320805a3
Ben Szekeres <benjamin.a.szekeres@gmail.com>
2024-03-31 08:52:59 +0200

    add main.py with comments outlining next steps

---
+++ src/main.py
@@ -0,0 +1,21 @@
+"""This module implements the Monte Carlo simulation for the stock price of ASML.
+
+Example use:
+    python3 main.py
+"""
+
+# import statements
+
+# load data
+
+# compute stock price returns
+
+# compute mean and standard deviation
+
+# set up any other simulation parameters
+
+# construct simulation paths
+
+# analysis and visualisation
+
+# argparse (optional)
