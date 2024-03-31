baa9d7af7af2d1ad9316ec14f85a555580597cd4
Ben Szekeres <benjamin.a.szekeres@gmail.com>
2024-03-31 09:01:48 +0200

    set up argument parser with basic simulation parameters

--- src/main.py
+++ src/main.py
@@ -18,4 +18,22 @@ Example use:
 
 # analysis and visualisation
 
-# argparse (optional)
+
+def main(args):
+    pass
+
+if __name__ == '__main__':
+    # Instantiate the parser
+    import argparse
+    parser = argparse.ArgumentParser(description='Monte Carlo Simulation')
+
+    # TODO: add any other command line arguments (i.e. share price history to consider)
+    parser.add_argument('--days', '-d', type=int,
+                         help='Number of future days to simulate', default=365)
+    parser.add_argument('--iterations', '-i', type=int,
+                         help='Number of simulation paths', default=1000)
+    args = parser.parse_args()
+    print(vars(args))
+    
+    # Run main with the arguments passed
+    main(args)
